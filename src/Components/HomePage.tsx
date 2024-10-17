747d37bfba8e2175e785f7c28efd87cab4dd4973
temp <temp@airfryer.local>
2024-10-17 01:06:38 -0400

    progress 3

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,15 +1,24 @@
 import '../Styles/HomePage.css';
 import Headshot from '../Assets/Images/Untitled-1-Recovered_2.png';
+import navIcon1 from '../Assets/Images/nav-icon1.svg';
+import navIcon2 from '../Assets/Images/nav-icon2.svg';
+import navIcon3 from '../Assets/Images/nav-icon3.svg';
  
 
 export default function HomePage() {
     return (
-        <div>
+        <div className='parent'>
         
-             <div className="name">Hi, I'm&nbsp;<span className='roles'>Kevin!</span></div>
+             <div className="name">Hi, It's&nbsp;<span className='kevin'>Kevin!</span></div><span className='roles'>I'm a&nbsp;<span></span></span>
              <div className='caption'>As a student at Northeastern University pursuing a bachelor's degree in computer science, I am passionate about leveraging my technical knowledge to drive positive change in the tech industry. My interests lies in web and app development and I aspire to be a frontend or full stack developer.  </div>
 
-             <span className="connect" ><button className="vvd"><span>Lets connect!</span></button>                  <img className='headshot' src={Headshot} alt=""/></span>  
+             <span className="connect" ><button className="vvd" ><span>Lets connect!</span></button>      
+             <div className="social-icon" style={{paddingLeft: 10}}>
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
+              </div>            
+             <img className='headshot' src={Headshot} alt=""/></span>  
     
 
         </div>
